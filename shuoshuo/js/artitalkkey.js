4cde023cede00b14c3e832aa920357876481ab47
bianshujun <1938533005@qq.com>
2020-12-11 21:41:41 +0800

    Site updated: 2020-12-11 21:41:39

---
+++ shuoshuo/js/artitalkkey.js
@@ -0,0 +1,15 @@
+new Artitalk({
+    appId: '8iW7PzPK2eGFNa0WgGzQqxnf-MdYXbMMI',
+    appKey: '2EBkKdNT1qMJzngfJRgriXSw',
+	pageSize: 1,
+	color1: "#b30070",
+	color2: "rgb(229, 91, 178)",
+});
+$(".power").css("padding","10px");
+$(".power").css("text-align","left");
+$("#switchUser").css("padding-right","10px");
+$("#pubShuo").css("padding-right","10px");
+$("#pubComment").css("padding-right","10px");
+$("#uploadSource").css("padding-right","10px");
+
+
