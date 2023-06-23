fc9fa1665d9ac7a2a83a56f8232e3785d8d2d432
bianshujun <bsj1218@163.com>
2023-06-23 15:38:19 +0800

    Site updated: 2023-06-23 15:38:16

---
+++ js/icon.js
@@ -0,0 +1,8 @@
+$(".float-flag").remove()
+
+$(".card-info-social-icons").append(
+    '<a class="social-icon float-flag" href="https://github.com/byxiaobian/byxiaobian.github.io.git" target="_blank"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-QQ"></use></svg></a>',
+    '<a class="social-icon float-flag" href="mailto:1938533005@qq.com" target="_blank"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-email"></use></svg></a>',
+    '<a class="social-icon float-flag" href="https://qm.qq.com/cgi-bin/qm/qr?k=ZSmJoH-VHg18VhBQWDQlrbBY5RiQQXyE&noverify=0#" target="_blank"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-github"></use></svg></a>',
+    '<a class="social-icon float-flag" href="/atom.xml" target="_blank"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-rss"></use></svg></a>'
+);
